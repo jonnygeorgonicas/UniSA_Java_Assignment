8d4e5e4aabb880cfa2b40a4da814b59a6e21acfe
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-03-26 14:25:30 +1030

    added inheritance from Item to Food

--- assignment_java/src/students/items/Food.java
+++ assignment_java/src/students/items/Food.java
@@ -1,6 +1,20 @@
 package students.items;
 
-public class Food {
+public abstract class Food extends Item {
 
+    @Override
+    public int tick() {
+        return super.tick();
+    }
+
+    @Override
+    public void setAge(int temp_age) {
+        super.setAge(temp_age);
+    }
+
+    @Override
+    public String died() {
+        return super.died();
+    }
 
 }
