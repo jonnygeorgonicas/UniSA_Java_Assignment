9955ebd1e6b074d2524bf22076a0a01e23002dbf
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-03-26 14:20:03 +1030

    Update Item.java to only include died and have less getters and setters

--- assignment_java/src/students/items/Item.java
+++ assignment_java/src/students/items/Item.java
@@ -5,45 +5,24 @@ public abstract class Item {
 	private int mat_age = 0;
 	private int death_age = 0;
 	private int montery_val = 0;
-	
-	public int getMat_age() {
-		return mat_age;
-	}
 
-	public void setMat_age(int mat_age) {
-		this.mat_age = mat_age;
-	}
+	public int tick() {
+		this.age += 1;
 
-	public int getDeath_age() {
-		return death_age;
-	}
-
-	public void setDeath_age(int death_age) {
-		this.death_age = death_age;
-	}
-
-	public int getMontery_val() {
-		return montery_val;
-	}
-
-	public void setMontery_val(int montery_val) {
-		this.montery_val = montery_val;
-	}
-
-	public int getAge() {
 		return age;
 	}
-	
+
 	public void setAge(int temp_age) {
 		this.age = temp_age;
 	}
-	
-	public int tick() {
-		this.age += 1;
-		
-		return age;
+
+	public String died() {
+		if (age > death_age) {
+			return "Food Item is dead!";
+		} else
+			return "Food Item isn't dead!";
 	}
-	
+
 	public int getValue() {
 		if (age > mat_age) {
 			return montery_val;
@@ -64,5 +43,5 @@ public abstract class Item {
 	}
 
 	public abstract String toString();
-	
+
 }
