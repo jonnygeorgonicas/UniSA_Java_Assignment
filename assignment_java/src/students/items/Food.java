c00945586d05dc382bf84feeefccec9b045f922d
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-03-26 14:43:39 +1030

    Update Food.java

--- assignment_java/src/students/items/Food.java
+++ assignment_java/src/students/items/Food.java
@@ -1,30 +1,15 @@
 package students.items;
 
 public abstract class Food extends Item {
+    private int cost = 0;
 
-    @Override
-    public int tick() {
-        return super.tick();
+    public int getCost() {
+        return cost;
     }
 
-    @Override
-    public void setAge(int temp_age) {
-        super.setAge(temp_age);
-    }
-
-    @Override
-    public String died() {
-        return super.died();
-    }
-
-    @Override
-    public int getValue() {
-        return super.getValue();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public void setCost(int cost) {
+        this.cost = cost;
     }
 
 }
+// up casting and down casting for the assignment is needed
