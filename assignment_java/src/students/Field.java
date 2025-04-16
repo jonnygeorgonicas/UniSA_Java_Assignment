2358f9bbc4086b0c9a2864815ccaf991d9a33923
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-04-16 23:40:23 +0930

    Update Field.java with declared data types

--- assignment_java/src/students/Field.java
+++ assignment_java/src/students/Field.java
@@ -1,7 +1,17 @@
 package students;
+import students.items.*;
 
-
-public class Field {
+public class Field extends Item{
+	Item[][] fieldArray;
+	Soil s = new Soil();
+	Weed w = new Weed();
+	Apples a = new Apples();
+	Grain g = new Grain();
+	UntilledSoil us = new UntilledSoil();
+	int appleCreate;
+	int grainCreate;
+	int appleCount;
+	int grainCount;
 	
 	public Field(int height, int width)
 	{
