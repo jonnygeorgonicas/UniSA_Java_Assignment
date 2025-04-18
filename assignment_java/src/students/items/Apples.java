ffbd145f5eead54576e0677a56c9b5b38c92b88d
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-04-18 23:57:23 +0930

    Update Apples.java

--- assignment_java/src/students/items/Apples.java
+++ assignment_java/src/students/items/Apples.java
@@ -13,21 +13,22 @@ public class Apples extends Food {
 		death_age = 5;
 		montery_val = 3;
 		age = 0;
+		//sets the cost to 2
 		super.setCost(2);
 		
 		counter++;
 	}
-
+	//increases the age of the grain when called
     public int tick() {
 		this.age += 1;
 
 		return age;
     }
-
+  //sets the age of the apple
     public void setAge(int temp_age) {
         age = temp_age;
     }
-
+ // checks if the age is above the death age to see if the apple is dead
     public boolean died() {
         if (this.age > this.death_age) {
             return true;
@@ -36,18 +37,19 @@ public class Apples extends Food {
         }
 
     }
-
+  //returns the value of the object if the age is greater than the mat_age
     public int getValue() {
-    	if (this.age > this.getMat_age()) {
+    	if (this.age > this.mat_age) {
 			return this.montery_val;
 		}
 		return 0;
     }
-
+  //returns 'A' if age is above mat_age
     public String toString() {
-        if (this.age > this.getMat_age()) {
+        if (this.age > this.mat_age) {
             return "A";
         }
+      //otherwise returns 'a'
         return "a";
     }
 
@@ -55,18 +57,10 @@ public class Apples extends Food {
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
-    
+  //returns the number of instances generated
     public static int getGenerationCount() {
     	return counter;
     }
-
-	public int getMat_age() {
-		return mat_age;
-	}
-
-	public void setMat_age(int mat_age) {
-		this.mat_age = mat_age;
-	}
 
 }
 
