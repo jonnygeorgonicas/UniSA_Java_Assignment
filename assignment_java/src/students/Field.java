1ebaa0308b26c809480c190756aae61b2d10b2cf
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-04-18 23:56:57 +0930

    Update Field.java
    fixed array issue and added Plague method

--- assignment_java/src/students/Field.java
+++ assignment_java/src/students/Field.java
@@ -1,6 +1,10 @@
 package students;
 import students.items.*;
 
+import java.util.Random;
+
+import students.Farm;
+
 public class Field extends Item{
 	Item[][] fieldArray;
 	Soil s = new Soil();
@@ -23,7 +27,7 @@ public class Field extends Item{
 		this.fieldArray = new Item[height][width];
 		
 		for (int i = 0; i < fieldArray.length; i++) {
-			for (int j = 0; j < fieldArray.length; j++) {
+			for (int j = 0; j < fieldArray[i].length; j++) {
 				fieldArray[i][j] = s;
 		
 			}
@@ -32,7 +36,7 @@ public class Field extends Item{
 	
 	public int tick() {
 		for (int i = 0; i < fieldArray.length; i++) {
-			for (int j = 0; j < fieldArray.length; j++) {
+			for (int j = 0; j < fieldArray[i].length; j++) {
 				//ticks every item in the field
 				fieldArray[i][j].tick();
 				
@@ -76,9 +80,11 @@ public class Field extends Item{
 		fieldArray[pos1][pos2] = item;
 		
 		if (fieldArray[pos1][pos2] instanceof Apples) {
+			//adds 1 to the appleCreate int to check for the number of apples created by the user
 			appleCreate += 1;
 		}
 		if (fieldArray[pos1][pos2] instanceof Grain) {
+			//adds 1 to the grainCreate int to check for the number of grains created by the user
 			grainCreate += 1;
 		}
 	}
@@ -89,7 +95,7 @@ public class Field extends Item{
 		int count = 0;
 		
 		for (int i = 0; i < fieldArray.length; i++) {
-			for (int j = 0; j < fieldArray.length; j++) {
+			for (int j = 0; j < fieldArray[i].length; j++) {
 				//calculates sum of the entire field 
 				sum += fieldArray[i][j].getValue();
 				
@@ -108,6 +114,49 @@ public class Field extends Item{
 		}
 		//returns the total monetary value of each item in the field
 		return sum;
+	}
+	public void Plague() {
+		int cropNum = 0;
+		Random rand = new Random();
+		
+		for (int i = 0; i < fieldArray.length; i++) {
+			for (int j = 0; j < fieldArray[i].length; j++) {
+				if (fieldArray[i][j] instanceof Apples || fieldArray[i][j] instanceof Grain) {
+				cropNum++;
+				}
+			}
+		}
+		//if no instance of Apples or Grain exists on the field there will be no crops to be infected
+		if (cropNum == 0) {
+			System.out.println("There is no crops to infect!");
+			System.out.println("Try planting some apples or grain");
+			return; //exits method early
+		}
+		
+		//get the total cells 
+		int cells_tot = fieldArray.length * fieldArray[0].length;
+		//makes the double a int to find cells in infected
+		int cells_infected = (int)(cells_tot * (0.2 + Math.random() * 0.2));
+		//prints to console how many cells are infected
+		System.out.println("Infection has hit " + cells_infected + " crops will be affected.");
+		
+		int effected = 0;
+		//keeps looping until effected is greater than cells_infected
+		while (effected < cells_infected) {
+			
+			//gets random position within the array
+			int k = rand.nextInt(fieldArray.length);
+			int j = rand.nextInt(fieldArray[0].length);
+			
+			//checks if the element is an instance of Apples or Grain and makes it UntilledSoil
+			if (fieldArray[k][j] instanceof Apples || fieldArray[k][j] instanceof Grain) {
+				
+				fieldArray[k][j] = us;
+				
+				//effected increments up an value
+				effected++;
+			}
+		}
 	}
 
 	@Override
@@ -129,21 +178,21 @@ public class Field extends Item{
 	    for (int row = 0; row < height; row++) {
 	        sb.append(String.format("%2d ", row + 1)); // Row label
 	        for (int col = 0; col < width; col++) {
-	            sb.append(fieldArray[row][col] + "  "); // Placeholder for object representation
+	            sb.append(fieldArray[row][col] + "  "); // Placeholder for object representation, which might need to be used again to fix a error
 	        }
 	        sb.append("\n");
 	    }
 
-	    return sb.toString();
+	    return sb.toString(); // returns null instead of nothing for areas of the string that 
 	}
 
 	public String getSummary() {
 		int soilCount = 0;
 		int untilledCount = 0;
 		int weedCount = 0;
-		
+		//checks for instance of Soil, weed and untilledsoil to count them
 		for (int i = 0; i < fieldArray.length; i++) {
-			for (int j = 0; j < fieldArray.length; j++) {
+			for (int j = 0; j < fieldArray[i].length; j++) {
 				if (fieldArray[i][j] instanceof Soil) {
 					soilCount += 1;
 				}
@@ -155,7 +204,7 @@ public class Field extends Item{
 				}
 			}
 		}
-		
+		//provides summary of field
 		StringBuilder sb = new StringBuilder();
 		sb.append("Apples:        " + appleCount + "\n");
 		sb.append("Grain:         " + grainCount + "\n");
