de7c42d815ba3f64c953e3ea9d92e22eecff7f2f
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-04-18 21:44:42 +0930

    Update Apples.java
    got rid of the unnecessary int cost and used a setter in its place

--- assignment_java/src/students/items/Apples.java
+++ assignment_java/src/students/items/Apples.java
@@ -5,7 +5,6 @@ public class Apples extends Food {
 	private int death_age;
 	private int montery_val;
 	private int age;
-	private int cost;
 	
 	private static int counter;
 	
@@ -14,7 +13,7 @@ public class Apples extends Food {
 		death_age = 5;
 		montery_val = 3;
 		age = 0;
-		cost = 2;
+		super.setCost(2);
 		
 		counter++;
 	}
@@ -29,24 +28,24 @@ public class Apples extends Food {
         age = temp_age;
     }
 
-    public String died() {
+    public boolean died() {
         if (this.age > this.death_age) {
-            return "True";
+            return true;
         } else {
-            return "False";
+            return false;
         }
 
     }
 
     public int getValue() {
-    	if (this.age > this.mat_age) {
+    	if (this.age > this.getMat_age()) {
 			return this.montery_val;
 		}
 		return 0;
     }
 
     public String toString() {
-        if (this.age > this.mat_age) {
+        if (this.age > this.getMat_age()) {
             return "A";
         }
         return "a";
@@ -60,6 +59,14 @@ public class Apples extends Food {
     public static int getGenerationCount() {
     	return counter;
     }
+
+	public int getMat_age() {
+		return mat_age;
+	}
+
+	public void setMat_age(int mat_age) {
+		this.mat_age = mat_age;
+	}
 
 }
 
