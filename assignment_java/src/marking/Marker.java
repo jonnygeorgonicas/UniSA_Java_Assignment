3d4bd90e2b6d098c6b66d41a6440902b795678b1
jonnygeorgonicas <elyjj001@mymail.unisa.edu.au>
2025-04-18 23:55:40 +0930

    Update Marker.java
    committed current code for my Marker.java file

--- assignment_java/src/marking/Marker.java
+++ assignment_java/src/marking/Marker.java
@@ -1,8 +1,7 @@
 package marking;
-
-import students.Farm;
 import students.Field;
 import students.items.*;
+import students.Farm;
 
 public class Marker {
 
@@ -11,7 +10,7 @@ public class Marker {
 		// Section ONE
 		
 		//you should NOT be able to make an Item - following line shouldn't compile if uncommented
-		//new Item(0, 0, 0);
+//		new Item(0, 0, 0);
 
 		System.out.println(Apples.getGenerationCount()); // should print 0
 		Apples a = new Apples();
@@ -32,8 +31,7 @@ public class Marker {
 
 		// Section TWO
 		// uncomment this section once you get to it
-		
-		/*
+	
 		Field testField = new Field(5, 5);
 		System.out.println(testField.get(3, 3)); //should print "."
 		System.out.println(testField); //should print the field
@@ -46,13 +44,13 @@ public class Marker {
 		testField.till(0, 0);
 		System.out.println(a.equals(testField.get(0, 0))); //should print false
 		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		*/
+		
 
 		// Section THREE
 		// uncomment this section once you get to it
 		//remember, your farm should work on varies size fields (assuming all fields are less than 10x10)
-		//Farm f = new Farm(10, 5, 10);
-		//f.run();
+		Farm f = new Farm(10, 5, 10);
+		f.run(f);
 		
 	}
 
